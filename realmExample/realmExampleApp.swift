393505759e73fb5a614365da7a0190307ad3dcac
amolon615 <114776102+amolon615@users.noreply.github.com>
2023-01-05 15:24:32 +0100

    Initial Commit

---
+++ realmExample/realmExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  realmExampleApp.swift
+//  realmExample
+//
+//  Created by Artem on 05/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct realmExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
